dddaf56c523306ece577abd5786da847e09f794d
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-03-19 22:53:49 -0500

    programming a chat with c++ and sockets

---
+++ chat/client.cpp
@@ -0,0 +1,65 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+
+int main(int argc, char const *argv[]){
+    struct sockaddr_in stSockAddr;
+    int Res;
+    int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int n;
+ 
+    if (-1 == SocketFD){
+        perror("Can not create socket");
+        exit(1);
+    }
+ 
+    memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+ 
+    stSockAddr.sin_family = AF_INET;
+    stSockAddr.sin_port = htons(5555);
+    Res = inet_pton(AF_INET, "127.0.0.1", &stSockAddr.sin_addr);
+ 
+    if (0 > Res){
+        perror("Error: first parameter is not a valid address family");
+        close(SocketFD);
+        exit(1);
+    }
+    else if (0 == Res){
+        perror("Char string (second parameter does not contain valid ipaddress");
+        close(SocketFD);
+        exit(1);
+    }
+ 
+    if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in))){
+        perror("Connect failed");
+        close(SocketFD);
+        exit(1);
+    }
+
+    int buffer_size = 256;
+    char buffer[buffer_size];
+
+    std::string text = "";
+    do{
+        bzero(buffer,buffer_size);                      // pull zero to the buffer
+        std::cout << "[Client]:\t";
+        getline(std::cin, text);
+
+        n = write(SocketFD, text.c_str(), text.size()+1);
+
+        n = read(SocketFD, buffer, buffer_size-1);
+        printf("[Server]: %s\n",buffer);
+
+    } while(text != "bye");    
+ 
+    shutdown(SocketFD, SHUT_RDWR);
+ 
+    close(SocketFD);
+    return 0;
+  }
---
+++ chat/server.cpp
@@ -0,0 +1,75 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+
+int main(int argc, char const *argv[]){
+	struct sockaddr_in stSockAddr;								// Address Socket
+	int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);	// File Descriptor
+	int buffer_size = 256;			// Buffer Lenght
+    char buffer[buffer_size];		// Create Buffer
+    int n;							// Current Buffer Lenght
+
+    if(-1 == SocketFD){
+    	perror("Can not create socket");
+    	exit(1);
+    }
+
+    memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+ 
+    stSockAddr.sin_family = AF_INET;
+    stSockAddr.sin_port = htons(5555);
+    stSockAddr.sin_addr.s_addr = INADDR_ANY;
+	
+	// Validate bind socket address
+    if(-1 == bind(SocketFD,(const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in))){
+		perror("Error bind failed");
+		close(SocketFD);
+		exit(1);
+    }
+ 
+ 	// Validate if the socket is listening
+    if(-1 == listen(SocketFD, 10)){
+		perror("Error listen failed");
+		close(SocketFD);
+		exit(1);
+    }
+
+ 	std::string text = "";
+    do{
+		int ConnectFD = accept(SocketFD, NULL, NULL);
+ 
+		if(0 > ConnectFD){
+        	perror("Error accept failed");
+        	close(SocketFD);
+        	exit(0);
+		}
+ 
+ 
+		bzero(buffer,buffer_size);						// put zero to the buffer
+		n = read(ConnectFD, buffer, buffer_size-1);		// read msg in the buffer
+
+		if (n < 0)		
+			perror("ERROR reading from socket");
+		
+		printf("[Client]: %s\n",buffer);
+
+		std::cout << "[Server]:\t";
+		getline(std::cin, text);		
+
+		n = write(ConnectFD, text.c_str(), text.size()+1);
+		if (n < 0) perror("ERROR writing to socket");
+ 
+		shutdown(ConnectFD, SHUT_RDWR);
+ 		close(ConnectFD);
+    } while(text != "bye");
+
+    close(SocketFD);
+
+	return 0;
+}
