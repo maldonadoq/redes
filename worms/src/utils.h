4bbcacaf0126c4b343758f26c7c934229386c177
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-04-09 09:56:44 -0500

    print tabñe

---
+++ worms/src/utils.h
@@ -0,0 +1,33 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+void print_table(char **_table, unsigned _r, unsigned _c){
+	for(unsigned i=0; i<_r; i++){
+		for(unsigned j=0; j<_c; j++)
+			std::cout << _table[i][j];
+		std::cout << "\n";
+	}
+}
+
+void print_table_from_str(std::string _t, unsigned _r, unsigned _c){
+	// std::cout << _t << "\n";
+	for(unsigned i=0; i<_r; i++){
+		for(unsigned j=0; j<_c; j++)
+			std::cout << _t[(i*_c)+j];
+		std::cout << "\n";
+	}
+}
+
+std::string table_to_str(char **_t, unsigned _r, unsigned _c){
+	std::string tmp = "";
+
+	for(unsigned i=0; i<_r; i++){
+		for(unsigned j=0; j<_c; j++){
+			tmp += _t[i][j];
+		}						
+	}
+
+	return tmp;
+}
+
+#endif
