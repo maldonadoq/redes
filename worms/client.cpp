b88acf9cfdea547514bf83caee48782824613a05
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-04-09 09:53:01 -0500

    server main and client main

--- worms/client.cpp
+++ worms/client.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include "src/client.h"
 
+unsigned rsize = 10;
+unsigned csize = 50;
+
 int main(int argc, char const *argv[]){
 		
-	std::string ip = "127.0.0.1";
+	std::string ip = "192.168.1.41";	
 	int port = 8888;
+	int speed = 750;
 
 	TClient *cl = new TClient();
 	cl->Connect(ip, port);
-	cl->Talking();
+	cl->Talking(speed);
 
 	delete cl;
 	return 0;
--- worms/server.cpp
+++ worms/server.cpp
@@ -3,11 +3,15 @@
 
 int main(int argc, char const *argv[]){
 	int port = 8888;
+	unsigned rsize = 10;
+	unsigned csize = 50;
 
 	TServer *s = new TServer();
 
 	s->Connect(port);
-	s->Listening();
+	s->CreateTable(rsize, csize);
+
+	s->Listening();		// loop
 
 	delete s;
 	return 0;
