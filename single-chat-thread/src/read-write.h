3a0db1b1d2d4df7df908b287174b0f93a65fb0f6
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-04-13 13:19:02 -0500

    read - write

--- single-chat-thread/src/read-write.h
+++ single-chat-thread/src/read-write.h
@@ -5,28 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include "../../utils/util.h"
 
 #define N 2 			//	[0-99]
 #define BS_MAX	102		//	buffer size max + N 
-
-std::string to_str(int n, unsigned s){
-	std::string str = std::to_string(n);
-
-	for(unsigned i=str.size(); i<s; i++){
-		str = '0' + str;
-	}
-
-	return str;
-}
-
-std::string read_buffer(std::string _str, int _init, int _end){
-	std::string str = "";
-	for(int i=_init; i<_end; i++){
-		str += _str[i];
-	}
-
-	return str;
-}
 
 void thread_write(int _sockFD){
 	std::string text;
