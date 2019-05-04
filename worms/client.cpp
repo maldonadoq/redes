4b62410adf54518fa54def1e41b1134ef31ecde9
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-05-04 14:04:05 -0500

    speed player

--- worms/client.cpp
+++ worms/client.cpp
@@ -7,9 +7,9 @@ unsigned csize = 50;
 // build g++ -std=c++11 -pthread client.cpp -o client.out
 int main(int argc, char const *argv[]){
 		
-	std::string ip = "192.168.1.41";	
+	std::string ip = "127.0.0.1";
 	int port = 8888;
-	int speed = 750;
+	int speed = 250;
 
 	TClient *cl = new TClient();
 	cl->Connect(ip, port);
