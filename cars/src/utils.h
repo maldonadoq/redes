42ed0523cc4b93bf268356a2cfde551ba70bc18b
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-05-04 13:08:48 -0500

    getch function in linux with header termios.h, because getch() doesn't exist in windows

--- cars/src/utils.h
+++ cars/src/utils.h
@@ -2,6 +2,32 @@
 #define _UTILS_H_
 
 #include <iostream>
+#include <termios.h>
+#include <stdio.h>
+
+static struct termios told, tnew;
+
+void initTermios() {
+	tcgetattr(0, &told);
+	tnew = told;
+	tnew.c_lflag &= ~ICANON;
+	tnew.c_lflag &= ~ECHO;
+	tcsetattr(0, TCSANOW, &tnew);
+}
+
+void resetTermios() {
+	tcsetattr(0, TCSANOW, &told);
+}
+
+char getch(){
+	char ch;
+	initTermios();
+
+	ch = getchar();
+
+	resetTermios();
+	return ch;
+}
 
 void print_table_from_str(std::string _t, unsigned _r, unsigned _c){
 	// std::cout << _t << "\n";
