044e749c928fea022d4a38cd1642400552643d70
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-05-25 17:57:38 -0500

    Class Peer

--- p2p/inc/peer.h
+++ p2p/inc/peer.h
@@ -12,6 +12,7 @@
 
 #include "protocol.h"
 #include "peer-info.h"
+#include "utils.h"
 
 class TPeer{
 private:
@@ -24,6 +25,9 @@ private:
     static void KeepAlive(int);
     static void PeerLeft(TPeerInfo);
     static void PeerJoin(TPeerInfo);
+    static void Testing();
+    static void Listening();
+    static void PrintPeers();
 
     void GetPeerList();
 public:
@@ -84,10 +88,73 @@ void TPeer::LogIn(std::string _ip, int _port){
 void TPeer::KeepAlive(int _sleep){
     std::string text;
     TProtocol mtcp(m_bits_size);
-    while(true){        
+    while(m_peer_sock > 0){
         text = "on";
         mtcp.Sending(text, m_peer_sock, "K");
         std::this_thread::sleep_for(std::chrono::milliseconds(_sleep));
+    }
+}
+
+void TPeer::PrintPeers(){
+    std::cout << "list of peers ip: ";
+    for(unsigned i=0; i<m_peers.size(); i++){
+        std::cout << m_peers[i].m_ip << " ";
+    }
+    std::cout << "\n";
+}
+
+void TPeer::Listening(){
+    TProtocol mtcp(m_bits_size);
+    std::string text;
+    bool tstate = true;
+
+    TPeerInfo pinfo;
+    while(tstate){
+        text = mtcp.Receiving(m_peer_sock);
+        switch(text[0]){
+            case 'O':
+            case 'o':{
+                tstate = false;
+                break;
+            }
+            case 'R':
+            case 'r':{
+                std::cout << "\nremove: " << text.substr(1) << "\n";
+                pinfo = {text.substr(1), 0};
+                PeerLeft(pinfo);
+                PrintPeers();
+                break;
+            }
+            case 'J':
+            case 'j':{
+                std::cout << "\njoin: " << text.substr(1) << "\n";
+                // excluir la misma ip
+                pinfo = {text.substr(1), 0};
+                PeerJoin(pinfo);
+                PrintPeers();
+                break;
+            }
+        }
+    }
+}
+
+void TPeer::Testing(){
+    TProtocol mtcp(m_bits_size);
+    char _type;
+    bool state = true;
+    while(state){
+        _type = getch();
+
+        switch(_type){
+            case 'O':
+            case 'o':{                
+                state = false;
+                mtcp.Sending("LogOut", m_peer_sock, "O");
+                std::cout << "LogOut\n";
+                m_peer_sock = -1;
+                break;
+            }
+        }
     }
 }
 
@@ -108,6 +175,7 @@ void TPeer::GetPeerList(){
         boost::split(result, rlist, boost::is_any_of("|")); 
 
         TPeerInfo pinfo;
+        std::cout << "list of peers: ";
         for(unsigned i=0; i<result.size(); i++){
             pinfo = {result[i], 0};
             m_peers.push_back(pinfo);
@@ -125,7 +193,7 @@ void TPeer::PeerJoin(TPeerInfo _peer){
 void TPeer::PeerLeft(TPeerInfo _peer){
     std::vector<TPeerInfo> peers_tmp;
     for(unsigned i=0; i<m_peers.size(); i++){
-        if(m_peers[i].m_ip == _peer.m_ip){
+        if(m_peers[i].m_ip != _peer.m_ip){
             peers_tmp.push_back(m_peers[i]);
         }
     }
@@ -136,9 +204,15 @@ void TPeer::PeerLeft(TPeerInfo _peer){
 void TPeer::Run(){
     int tsleep = 2500;
     std::thread talive(KeepAlive, tsleep);  // Keep Alive
+    std::thread ttest(Testing);             // Testing
+    std::thread tlistenng(Listening);       // Testing
 
     talive.join();
+    ttest.join();
+    tlistenng.join();
+
     close(m_peer_sock);
+    m_peers.clear();
 }
 
 
