337af77db6d7bd688a6cab833c3d81c21cd3b1b0
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-05-27 15:48:24 -0500

    Buffer Size => 64

--- p2p/peer.cpp
+++ p2p/peer.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char const *argv[]){
 	std::string ip = "127.0.0.1";
 	int port = 7777;
-	int bs = 16;
+	int bs = 64;
 
 	TPeer *cl = new TPeer(ip, port, bs);
 
--- p2p/tracker.cpp
+++ p2p/tracker.cpp
@@ -4,7 +4,7 @@
 // build: g++ tracker.cpp -o tracker.out -pthread
 int main(int argc, char const *argv[]){
 	int port = 7777;
-	int bs = 16;
+	int bs = 64;
 
 	TTracker *t = new TTracker(bs, port);
 	t->Execute();
