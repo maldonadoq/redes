f2ec8beb7cf892846bf78ed92e055e1eea4a835c
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-05-27 15:48:38 -0500

    Map find

--- p2p/test.cpp
+++ p2p/test.cpp
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include <map>
 
 using std::cout;
 using std::string;
@@ -38,6 +39,28 @@ int main(int argc, char const *argv[]){
 	int tip = getpid();
 	cout << tip << "\n";
 	cout << getIPAddress() << "\n";
+
+    std::map<char,int> mymap;
+    std::map<char,int>::iterator it;
+
+    mymap['a']=50;
+    mymap['b']=100;
+    mymap['c']=150;
+    mymap['d']=200;
+
+    it = mymap.find('b');
+    if (it != mymap.end()){
+        it->second = 4000;
+        std::cout << "find\n";
+    }
+    else{
+        std::cout << "not find\n";
+    }
+
+    std::cout << "a => " << mymap.find('a')->second << '\n';
+    std::cout << "b => " << mymap.find('b')->second << '\n';
+    std::cout << "c => " << mymap.find('c')->second << '\n';
+    std::cout << "d => " << mymap.find('d')->second << '\n';
 
 	return 0;
 }
