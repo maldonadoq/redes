b9d94be614661322ceac936e666e0855732b9931
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-05-26 20:06:35 -0500

    PeerToStr
    Convert Peer to String

--- p2p/inc/peer-info.h
+++ p2p/inc/peer-info.h
@@ -1,10 +1,19 @@
 #ifndef _PEER_INFO_H_
 #define _PEER_INFO_H_
 
+#include <string>
+
+using std::string;
+using std::to_string;
+
 struct TPeerInfo
 {
 	std::string m_ip;
 	int m_port;
 };
+
+string PeerToStr(TPeerInfo _peer){
+	return _peer.m_ip + "|" + to_string(_peer.m_port);
+}
 
 #endif
